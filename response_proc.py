15aa64a73912221bbe5ec730517db71546e75d29
Jessica Major <jessica.j.major@gmail.com>
2017-02-27 02:46:59 -0600

    Add initial parsing for csv file into array from google response form

--- response_proc.py
+++ response_proc.py
@@ -1,74 +1,29 @@
 #!/usr/bin/env python3
 
-import tkinter as tk
-import tkinter.filedialog as tkF
-from tkinter import messagebox
-import itertools
 import sys
+import csv
 
-root = tk.Tk()
-root.withdraw()
-root.update()
-read_file = tkF.askopenfilename()
-
-write_file = input("Enter output filename:")
-
-writing = "Writing - press releases, email, calling scripts, speeches"
-public = "Public Speaking"
-
-allSkillOptions = [writing, public] #...
-
-if write_file[-4:] != ".csv":
-	write_file = write_file + ".csv"
-
-print("Now converting " +read_file)
-
-responseLines = []
-
-try: 
-	with open(read_file, 'r') as rf: 
-
-		 
-		# Skip first lines: start=1, stop=None
-		for line in itertools.islice(rf, 1, None):  
-			person_response = line.split("\n")
-			#responseLines.append(split_line)
-			#print(person_response[0])
-			columns_in_response = person_response[0].split("\",\"")
-			for column in columns_in_response:
-				print(column)
-				print("\n")
-			print("\n")
-#				if (len(split_line) > 2):
-#					wf.write(split_line[2] + ",\n")
-	
-
-#print(responseLines)
-	print("\n\n\n\n")
-	for person in responseLines:
-		print(person)
-		print("\n")
-
-except IndexError:
-	print("Some line had the wrong # of columns. Unable to continue")
-except :
-	print("Unable to process this file. :(")
-	raise
+inputfile = 'response.csv'
 
 
-#with open(write_file, 'w') as wf:
-	#for line in itertools.islice(rf, 0, 1):
-#		wf.write(line)
+def parse_csv_into_array(csvfile):
+    parsedresponses = []
 
-		### write more output here
-#		print("All done! Output located at " + write_file + "\n")
+    with open(csvfile) as responses:
+        responsereader = csv.reader(responses, delimiter = '|')
+        for response in responsereader:
+            parsedresponses.append(response)
 
-
-
-
-
-input("Press any key to exit")
+    return parsedresponses
 
 
-	
+def main():
+    parsedresponses = parse_csv_into_array(inputfile)
+
+    print(parsedresponses)
+    print(len(parsedresponses))
+
+
+if __name__ == '__main__':
+    sys.exit(main())
 
